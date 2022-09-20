122ef9af4fe525d2193b2a04b350433a51676c11
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-20 08:50:56 -0300

    Assignment #11

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -3,6 +3,7 @@ window.alert("Hello, World!");
 document.write('Hello, World!');
 
 var A = " This is a string.<br><br>";
+var A = A.fontcolor("purple");
 document.write (A);
 
 document.write ("If you ask, \"What is the purpose of this page?\" <br> I would say, \"There is no purpose. And yet there is.\" ");
@@ -10,8 +11,13 @@ document.write ("If you ask, \"What is the purpose of this page?\" <br> I would 
 document.write("<br><br>\"If life were predictable" + " it would cease to be life," + " and would be without flavor.\"" + " -Eleanor Roosevelt");
 
 var text = "<br><br>This text is" + " concatenated," + " although you can\'t really tell.";
+var text = text.fontcolor("orange");
 document.write(text);
 
 var weather = "<br><br>Today, the weather is ", rain = "rainy.", sun = "sunny.", cloud = "cloudy.", mix = "a mix of sun and cloud.";
+var rain = rain.fontcolor("blue");
+var sun = sun.fontcolor("yellow");
+var cloud = cloud.fontcolor("grey");
+var mix = mix.fontcolor("red");
 document.write(weather);
-document.write(sun);
+document.write(mix);
