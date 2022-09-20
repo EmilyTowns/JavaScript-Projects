839d861aca9cc6f95fbe5242d5277ea11a69da78
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-20 08:24:49 -0300

    Assignment #9

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -7,3 +7,5 @@ var A = " This is a string.<br>";
 document.write (A);
 
 document.write ("If you ask, \"What is the purpose of this page?\" <br> I would say, \"There is no purpose. And yet there is.\" ");
+
+document.write("<br>\"If life were predictable" + " it would cease to be life," + " and would be without flavor.\"" + " -Eleanor Roosevelt")
