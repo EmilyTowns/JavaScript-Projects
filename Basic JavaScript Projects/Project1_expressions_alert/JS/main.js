8c7887f8f55f0ae15e2dc2138db7f1bcf2923809
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-20 08:30:15 -0300

    Challenge #1

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,10 +2,12 @@ window.alert("Hello, World!");
 
 document.write('Hello, World!');
 
-var A = " This is a string.<br>";
-
+var A = " This is a string.<br><br>";
 document.write (A);
 
 document.write ("If you ask, \"What is the purpose of this page?\" <br> I would say, \"There is no purpose. And yet there is.\" ");
 
-document.write("<br>\"If life were predictable" + " it would cease to be life," + " and would be without flavor.\"" + " -Eleanor Roosevelt")
+document.write("<br><br>\"If life were predictable" + " it would cease to be life," + " and would be without flavor.\"" + " -Eleanor Roosevelt");
+
+var text = "<br><br>This text is" + " concatenated," + " although you can\'t really tell.";
+document.write(text);
