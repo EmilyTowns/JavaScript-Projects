934033fe94998c78566e89e2f016a23c76660c5d
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-20 08:15:00 -0300

    Assignment #8

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,6 +2,8 @@ window.alert("Hello, World!");
 
 document.write('Hello, World!');
 
-var A = " This is a string.";
+var A = " This is a string.<br>";
 
 document.write (A);
+
+document.write ("If you ask, \"What is the purpose of this page?\" <br> I would say, \"There is no purpose. And yet there is.\" ");
