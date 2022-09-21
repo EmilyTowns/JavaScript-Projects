c980c84f773e4498eece0dbef27a5e8db253186c
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-21 08:24:23 -0300

    Assignment #13

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function Button_Function() {
+    var str = "Now I'm red!";
+    var result = str.fontcolor("red");
+    document.getElementById("Red_Text") .innerHTML = result;
+}
+
+function Mouse_Over_Function() {
+    var str = "Now I'm blue!";
+    var result = str.fontcolor("blue");
+    document.getElementById("Paragraph") .innerHTML = result;
+}
