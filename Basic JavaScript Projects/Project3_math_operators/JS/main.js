c818e6c0deb1bae49f9093e90aa4ac93c71274ff
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-23 10:05:20 -0300

    Assignment #17

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,8 @@ function multiplication() {
     var multiply = 70*120;
     document.getElementById("math").innerHTML="70 times 120 equals "+multiply;
 }
+
+function subtraction() {
+    var Sub = 82-70;
+    document.getElementById("subtract").innerHTML="82 minus 70 equals "+Sub;
+}
