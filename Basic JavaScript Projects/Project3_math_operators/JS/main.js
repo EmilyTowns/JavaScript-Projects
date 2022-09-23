25834de288bad7b30ee6c0e3f05b06f5ac27381f
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-23 10:43:42 -0300

    Assignment #20

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -17,3 +17,8 @@ function multiMath() {
     var math = ((2*10)+50)/7-2;
     document.getElementById("multi").innerHTML="2 times 10, plus 50, divided by 7, minus 2 equals "+math;
 }
+
+function modulusMath() {
+    var modu = 20%3;
+    document.getElementById("mod").innerHTML="The remainder of 20 divided by 3 is "+modu;
+}
