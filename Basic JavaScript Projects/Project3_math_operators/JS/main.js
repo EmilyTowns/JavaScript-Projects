01228248deef1a3f8e6d265304119fdb435dc5be
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-23 10:16:08 -0300

    Assignment #18

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -7,3 +7,8 @@ function subtraction() {
     var Sub = 82-70;
     document.getElementById("subtract").innerHTML="82 minus 70 equals "+Sub;
 }
+
+function division() {
+    var div = 90/3;
+    document.getElementById("divide").innerHTML="90 divided by 3 equals "+div;
+}
