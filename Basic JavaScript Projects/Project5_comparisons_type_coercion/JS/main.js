808ee0ab6dec8cfe570dfe308437144e7d937e62
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-26 10:56:11 -0300

    Assignment #27

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -19,3 +19,17 @@ function noNaN() {
         "\"506\" is not a number: " + 
         isNaN('506');
 }
+
+function infinityNumber() {
+    let x = 2E+309;
+    document.getElementById("infinity").innerHTML=
+        "\"2E309\" is considered " +
+        x;
+}
+
+function negativeInfinityNumber() {
+    let x = -3E+310;
+    document.getElementById("negativeInfinity").innerHTML=
+        "\"-3E310\" is considered " +
+        x;
+}
