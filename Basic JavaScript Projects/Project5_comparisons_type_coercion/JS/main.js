e22bfe9d485427d4376bbeb81847cb44c960ace6
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-26 10:36:10 -0300

    Challenge #6

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -7,3 +7,15 @@ function typeOfFunction() {
         "\"False\" is a " +
          typeof false;
 }
+
+function yesNaN() {
+    document.getElementById("NaN").innerHTML=
+        "\"George\" is not a number: " + 
+        isNaN('George');
+}
+
+function noNaN() {
+    document.getElementById("notNaN").innerHTML=
+        "\"506\" is not a number: " + 
+        isNaN('506');
+}
