abbfdb8ec7f5d94a255743b2a8d5eb55398f74c5
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-26 09:53:25 -0300

    Assignment #26

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,9 @@
+function typeOfFunction() {
+    document.getElementById("typeOf").innerHTML=
+        "\"Emily\" is a " +
+         typeof "Emily"+ "<br>" +
+        "\"25\" is a " +
+         typeof 25 + "<br>" +
+        "\"False\" is a " +
+         typeof false;
+}
