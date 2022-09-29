9290f0c4750b76abf55c52e0c368538bd529ae70
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-29 11:15:33 -0300

    Assignment #39

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,24 @@
+var X = 50
+function subtractFunction() {
+    var sub = X - 10;
+    document.getElementById("subtract").innerHTML=
+    "The variable X is 50. Minus 10 is " + sub;
+}
+function addFunction() {
+    var add = X + 10;
+    document.getElementById("add").innerHTML=
+    "The same variable plus 10 is " + add;
+}
+
+function localSubtract() {
+    var Y = 40;
+    var sub = Y - 10;
+    document.getElementById("lSub").innerHTML=
+    "The variable Y is 40. Minus 10 is " + sub;
+}
+function localAdd() {
+    var add = Y + 10;
+    document.getElementById("lAdd").innerHTML=
+    "The same variable plus 10 is " + add;
+    console.log(Y + 10);
+}
