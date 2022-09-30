14ad969f72031da03c205544262000cf32c0cc5c
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-30 10:54:11 -0300

    Challenge #10

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -12,3 +12,17 @@ function sliceFunction() {
     var Slice = Sentence.slice(20, 38);
     document.getElementById("slice").innerHTML = Slice;
 }
+
+function upperCaseFunction() {
+    var Sentence = "this is a sentence.";
+    var Case = Sentence.toUpperCase();
+    document.getElementById("upperCase").innerHTML=Case;
+}
+
+function searchFunction() {
+    var Sentence = "Today is a sunny day.";
+    var Search = Sentence.search("sunny");
+    document.getElementById("searchMethod").innerHTML=
+    "In the sentence \"Today is a sunny day.\", the word \"sunny\" is in position "
+    + Search;
+}
