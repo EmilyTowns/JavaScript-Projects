b9d439a23437728b3089ff9ad91fa2110167901a
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-09-30 10:32:39 -0300

    Assignment #45

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -6,3 +6,9 @@ function sentenceFunction() {
     var sentence = sent1.concat(sent2, sent3, sent4);
     document.getElementById("concatenate").innerHTML=sentence;
 }
+
+function sliceFunction() {
+    var Sentence = "This sentence needs some words cut out.";
+    var Slice = Sentence.slice(20, 38);
+    document.getElementById("slice").innerHTML = Slice;
+}
