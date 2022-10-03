fec3590ad60c28a97ce036879e0b7de5c5f9efac
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-10-03 12:50:09 -0300

    Challenge #13

---
+++ Basic JavaScript Projects/JS/challenge13.js
@@ -0,0 +1,6 @@
+function stringLengthFunction() {
+    var X = "Computer";
+    var Y = X.length;
+    document.getElementById("stringLength").innerHTML = 
+    "The length of the string \"Computer\" is: " + Y;
+}
