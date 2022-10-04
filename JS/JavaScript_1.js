32b25df7e34162419948a8cd7e20363cb83f5a9b
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-10-04 10:50:42 -0300

    Assignment #56

---
+++ JS/JavaScript_1.js
@@ -0,0 +1,28 @@
+function foodFunction() {
+    var Food_Output;
+    var Food = document.getElementById("Input").value;
+    var Food_String = ", yummy!";
+    switch(Food) {
+        case "Tacos":
+            Food_Output = "Tacos" + Food_String;
+        break;
+        case "Chicken pot pie":
+            Food_Output = "Chicken pot pie" + Food_String;
+        break;
+        case "Lasagna":
+            Food_Output = "Lasagna" + Food_String;
+        break;
+        case "Roast chicken":
+            Food_Output = "Roast chicken" + Food_String;
+        break;
+        case "Ham":
+            Food_Output = "Ham" + Food_String;
+        break;
+        case "Salad":
+            Food_Output = "Salad" + Food_String;
+        break;
+        default:
+        Food_Output = "Please enter a food exactly as written in the above list.";
+    }
+    document.getElementById("Output").innerHTML = Food_Output;
+}
