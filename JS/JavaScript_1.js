eddcb4fb6258874c58de742570373d3a02ad239e
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-10-05 09:15:14 -0300

    Challenge #17

--- JS/JavaScript_1.js
+++ JS/JavaScript_1.js
@@ -43,3 +43,15 @@ function canvasFunction(){
     ctx.fillStyle = grd;   //States that the fill of the canvas should be the radial gradient
     ctx.fillRect(0,0,500,250);   //Creates a rectangle with x, y, height, and width that will fill according to the fillstyle
 }
+
+function canvasFunction2(){
+    var c = document.getElementById("canvas2");   //Declares variable and assigns canvas element from HTML
+    var ctx = c.getContext("2d");   //Declares another variable and defines the drawing context as 2d rendering
+
+    var grd = ctx.createLinearGradient(0,0,250,0);   //Declares another variable as creating a radial gradient within the ctx context 
+    grd.addColorStop(0,"pink");   //Defines a new color stop, which adds an offset and color
+    grd.addColorStop(1,"purple");   //Defines second color stop with a second color
+
+    ctx.fillStyle = grd;   //States that the fill of the canvas should be the radial gradient
+    ctx.fillRect(0,0,500,250);   //Creates a rectangle with x, y, height, and width that will fill according to the fillstyle
+}
